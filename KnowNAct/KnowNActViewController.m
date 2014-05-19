3a2f56ee8be9e40806868b72dfade5fce61bdcf8
JitendraDeore <jitendra@nayamode.com>
2014-05-19 19:09:32 +0530

    Remove first commit

--- KnowNAct/KnowNActViewController.m
+++ KnowNAct/KnowNActViewController.m
@@ -18,10 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    
-    NSLog(@"This is KnownnAct App");
-    
 }
 
 - (void)didReceiveMemoryWarning
